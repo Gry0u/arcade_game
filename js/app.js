cbd19ebbb233b0c2afeb0656de96ac076704eb5e
Gry0u <gauthriou@protonmail.com>
2018-09-19 21:35:19 +0200

    Update comments

--- js/app.js
+++ js/app.js
@@ -1,49 +1,48 @@
-// Enemies our player must avoid
+// Enemies the player must avoid
 class Enemy {
-    constructor(initLoc=[0, 50], speed=100) {
+    constructor(initLoc, speed) {
       this.sprite = 'images/enemy-bug.png';// The image/sprite for our enemies
       this.x = initLoc[0];//initial horizontal position
       this.y = initLoc[1];//intial vertical position
       this.speed = speed;//speed
     }
 
-    // Update the enemy's position, required method for game
+    // Update the enemy's position
     // Parameter: dt, a time delta between ticks
     update(dt) {
-      // You should multiply any movement by the dt parameter
-      // which will ensure the game runs at the same speed for
-      // all computers.
+      // Any movement is multipled by dt to ensure the game runs at the same
+      //speed for all computers.
+      //Enemies are moving from left to right.
+      //Enemies come back at the top left after having reached the top right
       this.x =
       this.x + this.speed * dt >= 505 ?
       (this.x + this.speed * dt) % 505 :
       this.x + this.speed * dt ;
     }
 
-    // Draw the enemy on the screen, required method for game
+    // Draw the enemy on the screen
     render() {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 class Player {
     constructor(initLoc=[202, 374]) {
       this.sprite = 'images/char-boy.png';// The image/sprite for the player
       this.x = initLoc[0];//initial horizontal position
-      this.y = initLoc[1];//intial vertical position
+      this.y = initLoc[1];//initial vertical position
     }
 
-    // Update the player's position, required method for game
+    // Update the player's position
     // Parameter: dt, a time delta between ticks
     update() {} // empty because movement is managed by handleInput
 
-    // Draw the player on the screen, required method for game
+    // Draw the player on the screen
     render() {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    //handle key inputs
     handleInput(key) {
       switch (key) {
         case 'left':
@@ -76,21 +75,20 @@ class Player {
 }
 
 
-// Now instantiate your objects.
+//Instantiate objects.
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [];
-//Instatiate 3 enemies with a random speed and add it to the array
+//Instatiate 3 enemies with a random speed and add them to the array
 for (i = 1; i < 4; i++ ) {
   let enemy = new Enemy([101, i * 83 - 31], 50 + Math.random() * 200);
   allEnemies.push(enemy);
 }
 
-// Place the player object in a variable called player
+// Player object
 let player = new Player();
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listens for key presses. Sends the keys to the Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
